75ef6840fa42772695406ac06461b371aeb689fd
desaiankitb <desaiankitb@gmail.com>
2018-05-03 00:40:02 +0530

    update: code fixed and formatting issues

--- 04_tokenize_data.py
+++ 04_tokenize_data.py
@@ -1,12 +1,12 @@
 # tokenizer
->>> from pyspark.ml.feature import Tokenizer
->>> sentences_df = spark.createDataFrame([
+from pyspark.ml.feature import Tokenizer
+sentences_df = spark.createDataFrame([
      (1, "This is an introduction to Spark MLlib"),
      (2, "MLlib includes libraries for classification and regression"),
      (3, "It also contains supporting tools for pipelines")],
      ["id","sentence"])
 
->>> sentences_df.show(10,False)
+sentences_df.show(10,False)
 # +---+----------------------------------------------------------+
 # |id |sentence                                                  |
 # +---+----------------------------------------------------------+
@@ -15,9 +15,9 @@
 # |3  |It also contains supporting tools for pipelines           |
 # +---+----------------------------------------------------------+
 
->>> sent_token = Tokenizer(inputCol="sentence", outputCol="words")
->>> sent_tokenized_df = sent_token.transform(sentences_df)
->>> sent_tokenized_df.show(10,False)
+sent_token = Tokenizer(inputCol="sentence", outputCol="words")
+sent_tokenized_df = sent_token.transform(sentences_df)
+sent_tokenized_df.show(10,False)
 # +---+----------------------------------------------------------+------------------------------------------------------------------+
 # |id |sentence                                                  |words                                                             |
 # +---+----------------------------------------------------------+------------------------------------------------------------------+
